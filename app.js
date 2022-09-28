468a9330019a7d1d3113add387e15e18c0607b45
ritu2302 <regalritu2001@gmail.com>
2022-09-28 12:24:47 +0530

    numbers

--- app.js
+++ app.js
@@ -10,3 +10,10 @@ console.log("Hello world")
 console.log('d') //All characters are string in JS
 
 console.log(typeof "Hello world")
+
+//Numbers
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof 1555515465461515645415)
+console.log(typeof -1)
+console.log(typeof -1.2)
